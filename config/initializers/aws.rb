0b2ce24cb18959b996af97d1225d5a1bd775563d
today1028 <today1q1q@naver.com>
2016-05-30 09:27:28 +0000

    20160530

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIAJOWIADLFMSM4RW3Q',                        # required
-    aws_secret_access_key: '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',                        # required
+    aws_access_key_id:     '',                        # required
+    aws_secret_access_key: '',                        # required
     region:                'ap-northeast-2',                  # optional, defaults to 'us-east-1'
     endpoint:              'https://s3.ap-northeast-2.amazonaws.com' # optional, defaults to nil
   }
